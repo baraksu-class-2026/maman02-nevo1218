2fbcfd8471efc1055118ac3610e16f1bfaf6b8f9
baraksu <baraksu@gmail.com>
2026-01-13 23:17:49 +0200

    Sync HotelRoomMatanH5Tester.java from maman02-MatanH5

---
+++ HotelRoomMatanH5Tester.java
@@ -0,0 +1,13 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class HotelRoomMatanH5Tester {
+
+    @Test
+    public void test01() {
+
+    }
+
+}
+
