dac92bd1c86c13ae5a1309015635e8cf6bafbb5e
baraksu <baraksu@gmail.com>
2026-01-13 23:17:13 +0200

    Sync HotelElkanazarskyTester.java from maman02-Elkana-zarsky

---
+++ HotelElkanazarskyTester.java
@@ -0,0 +1,13 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class HotelElkanazarskyTester {
+
+    @Test
+    public void test01() {
+
+    }
+
+}
+
