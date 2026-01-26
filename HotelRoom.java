b86c9dea07ddc6233aa16c721e97f6b204572074
nevo1218 <haramatyne@gmail.com>
2026-01-26 19:14:38 +0200

    Update HotelRoom.java

--- HotelRoom.java
+++ HotelRoom.java
@@ -1,33 +1,97 @@
+public class HotelRoom {
+    private final int DEFAULT_ROOM_NUM = 999;
+    private final int DEFAULT_NUM_BEDS = 2;
+    private final int MIN_BEDS = 2;
+    private final int MAX_BEDS = 4;
+    private final int MIN_ROOM_NUM = 100;
+    private final int MAX_ROOM_NUM = 999;
 
-/**
- * Write a description of class HotelRoom here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class HotelRoom
-{
-    // instance variables - replace the example below with your own
-    private int x;
+    private int roomNum;
+    private int numBeds;
+    private boolean occupied;
+    private String guest;
 
-    /**
-     * Constructor for objects of class HotelRoom
-     */
-    public HotelRoom()
-    {
-        // initialise instance variables
-        x = 0;
+    public HotelRoom(int roomNum, int numBeds) {
+        if (roomNumOk(roomNum)) {
+            this.roomNum = roomNum;
+        } else {
+            this.roomNum = DEFAULT_ROOM_NUM;
+        }
+
+        if (numBedsOk(numBeds)) {
+            this.numBeds = numBeds;
+        } else {
+            this.numBeds = DEFAULT_NUM_BEDS;
+        }
+
+        this.occupied = false;
+        this.guest = "";
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    private boolean roomNumOk(int roomNum) {
+        return roomNum >= MIN_ROOM_NUM && roomNum <= MAX_ROOM_NUM;
+    }
+
+    private boolean numBedsOk(int numBeds) {
+        return numBeds >= MIN_BEDS && numBeds <= MAX_BEDS;
+    }
+
+    public int getRoomNum() { 
+        return roomNum; 
+    }
+    public int getNumBeds() { 
+        return numBeds;
+    }
+    public boolean isOccupied() { 
+        return occupied; 
+    }
+    public String getGuest() { 
+        return guest; 
+    }
+
+    public void setRoomNum(int roomNum) {
+        if (roomNumOk(roomNum)) {
+            this.roomNum = roomNum;
+        }
+    }
+
+    public void setNumBeds(int numBeds) {
+        if (numBedsOk(numBeds)) {
+            this.numBeds = numBeds;
+        }
+    }
+
+    public boolean equals(HotelRoom other) {
+        return this.roomNum == other.roomNum && this.numBeds == other.numBeds;
+    }
+
+    public boolean before(HotelRoom other) {
+        return this.roomNum < other.roomNum;
+    }
+
+    public boolean after(HotelRoom other) {
+        return other.before(this);
+    }
+
+    public boolean checkIn(String guest) {
+        if (!this.occupied) {
+            this.occupied = true;
+            this.guest = guest;
+            return true;
+        }
+        return false;
+    }
+
+    public void checkOut() {
+        this.occupied = false;
+        this.guest = "";
+    }
+
+    public String toString() {
+        if (!occupied) {
+            return "Room " + roomNum + ", " + numBeds + " Beds: Available";
+        } else {
+            return "Room " + roomNum + ", " + numBeds + " Beds: Occupied by " + guest;
+        }
     }
 }
